1994f23a18b443096233245fe5c66d7598723159
Naresh Yerra <nareshyerra2282@gmail.com>
2023-01-17 19:22:34 +0530

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+let parael = document.getElementById("para");
+let inputel = document.getElementById("input");
+let btnel = document.getElementById("btn");
+let submitbtn = document.getElementById("submit")
+let text = document.getElementById("textarea")
+
+let buttonel = document.getElementById("button")
+
+let textparael = document.getElementById("textpara")
+buttonel.onclick = function() {
+    parael.textContent = `Hi ${inputel.value} welcome to softpath`;
+    
+    
+
+}
+
+submitbtn.onclick = function(){
+textparael.textContent= `${text.value}`;
+
+
+}
+textparael.style.color= "red";
+
+
+
+
+
